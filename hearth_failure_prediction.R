afcf56183f68a4d0797688d69e891b65aafea85b
Scrayil <mattia.bennati@mailbox.org>
2024-06-09 10:37:14 +0200

    Added presentation

--- hearth_failure_prediction.R
+++ hearth_failure_prediction.R
@@ -7,6 +7,7 @@
 # The performance of individual models, such as Random Forest, GLMNet, and XGBoost,
 # is assessed, and their variable importance is analyzed.
 # The combined model's accuracy is evaluated and compared to the base models.
+# Data taken from: https://www.kaggle.com/datasets/aadarshvelu/heart-failure-prediction-clinical-records
 
 
 # CLEARING THE CURRENT ENVIRONMENT
